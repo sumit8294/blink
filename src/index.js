28d927b906aae6bb7be9c4e29ace10423f9baf12
Sumit <sumitrathore8294@gmail.com>
2023-06-14 15:42:02 +0530

    Remove reducer and store

--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 
-import store from './store/store';
-import {Provider} from 'react-redux';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Router>
-	  	<Provider store={store}>
+	  	
 	    	<App />
-	    </Provider>
+	    
     </Router>
   
 );
--- src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from '@reduxjs/toolkit';
-import usersReducer from './usersSlice';
-
-const rootReducer = combineReducers({
-	users: usersReducer,
-
-})
-
-export default rootReducer;
