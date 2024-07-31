fc64bd8f60343f55c37a5912d91c5968efd017da
sumit8294 <113183456+sumit8294@users.noreply.github.com>
2024-07-31 22:52:04 +0530

    Update Explore.js: Add Infinite scroll

--- src/components/explores/Explore.js
+++ src/components/explores/Explore.js
@@ -3,11 +3,34 @@ import ExplorePosts from './ExplorePosts';
 
 import { useMediaQuery } from 'react-responsive';
 import {mobileMediaQuery} from '../../ReactResponsiveQueries';
+import { useCallback } from 'react';
+
+import { getPosts, getPostStatus } from  '../../reducers/posts/postSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import useAuth from '../../hooks/useAuth';
 
 
 const Explore = () =>{
 
 	const isMobileOrTablet = useMediaQuery(mobileMediaQuery);
+
+	const dispatch = useDispatch();
+	
+	const { userId, token } = useAuth();
+
+	const postStatus = useSelector(getPostStatus)
+
+	const handleScroll = useCallback((event) => {
+    const { scrollTop, scrollHeight, clientHeight } = event.currentTarget;
+
+    if (scrollHeight - scrollTop <= clientHeight + 800) { // Added buffer for preloading
+      if (postStatus !== 'loading' ) {
+        
+        dispatch(getPosts({ token, userId, count: 10 }))
+          
+      }
+    }
+  }, [dispatch, token, userId, postStatus]);
 
 	return (
 		<>
@@ -15,7 +38,7 @@ const Explore = () =>{
 				?
 					<>
 
-						<div className="text-white h-screen overflow-y-auto justify-center bg-blink-black-1   ">
+						<div onScroll={handleScroll} className="text-white h-screen overflow-y-auto justify-center bg-blink-black-1   ">
 
 							{/* <Search /> */}
 
@@ -31,7 +54,7 @@ const Explore = () =>{
 
 							<div className=" my-2 text-white py-4 rounded-2xl text-white laptop-lg:mx-2 laptop-lg:bg-blink-black-2">
 
-								<div className="custom-scroll h-screen overflow-y-auto laptop-lg:px-4">
+								<div onScroll={handleScroll} className="custom-scroll h-screen overflow-y-auto laptop-lg:px-4">
 
 									{/* <Search /> */}
 
